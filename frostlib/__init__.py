623db9173b552bc877937fc2a5097e823944f08c
FreakX <dani11795@googlemail.com>
2010-04-17 03:25:10 +0200

    Progress Bar Klasse hinzugefügt^^
    Signed-off-by: FreakX <dani11795@googlemail.com>

--- frostlib/__init__.py
+++ frostlib/__init__.py
@@ -71,3 +71,33 @@ def edout(out):
 def eout(out):
     if LOG_LEVEL >= 3:
         print out
+
+
+class ProgressBar:
+    def __init__(self, duration, text):
+        self.duration = duration
+        self.prog_bar = '[]'
+        self.text = text
+        self.fill_char = '='
+        self.width = 40
+        self.__update_amount(0)
+   
+    def __update_amount(self, new_amount):
+        percent_done = int(round((new_amount / 100.0) * 100.0))
+        all_full = self.width - 2
+        num_hashes = int(round((percent_done / 100.0) * all_full))
+        self.prog_bar = '[' + self.fill_char * num_hashes + ' ' * (all_full - num_hashes) + ']'
+        pct_place = (len(self.prog_bar) / 2) - len(str(percent_done))
+        pct_string = '%i%%' % percent_done
+        self.prog_bar = self.prog_bar[0:pct_place] + \
+            (pct_string + self.prog_bar[pct_place + len(pct_string):])
+       
+    def update_time(self, elapsed_secs):
+        self.__update_amount((elapsed_secs / float(self.duration)) * 100.0)
+        self.prog_bar += "  " + str(self.text)
+
+    def __str__(self):
+        return str(self.prog_bar)
+       
+
+
