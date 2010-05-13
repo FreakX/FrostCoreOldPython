f55b4c08738c59dbefec6acbe38e16a85832a945
FreakX <dani11795@googlemail.com>
2010-05-13 16:01:11 +0200

    Fixx Hashchanger
    Signed-off-by: FreakX <dani11795@googlemail.com>

--- hashchanger.py
+++ hashchanger.py
@@ -1,9 +1,9 @@
 import frostlib
 frostlib_hash = frostlib.hash.GetHashofDirs("frostlib", 1)
-f = open("logon.ini", 'r')
+f = open("logon.cfg", 'r')
 data = f.readlines()
 f.close()
-f = open("logon.ini", 'w')
+f = open("logon.cfg", 'w')
 for lines in data:
     if not "FROSTLIB_HASH" in lines:
         f.write(lines)
@@ -12,10 +12,10 @@ for lines in data:
     print lines
 f.close
 
-f = open("world.ini", 'r')
+f = open("world.cfg", 'r')
 data = f.readlines()
 f.close()
-f = open("world.ini", 'w')
+f = open("world.cfg", 'w')
 for lines in data:
     if not "FROSTLIB_HASH" in lines:
         f.write(lines)
