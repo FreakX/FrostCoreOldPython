76822149b7d385c613e997c76ba53e3954f2ec1d
FreakX <dani11795@googlemail.com>
2010-05-17 23:20:45 +0200

    [9] Wieder Hashchanger Fixx
    Signed-off-by: FreakX <dani11795@googlemail.com>

--- hashchanger.py
+++ hashchanger.py
@@ -1,3 +1,4 @@
+# -*- coding: cp1252 -*-
 import frostlib
 frostlib_hash = frostlib.hash.GetHashofDirs("frostlib", 0)
 f = open("logon.cfg", 'r')
@@ -21,3 +22,21 @@ for lines in data:
     else:
         f.write("frostlib_hash = "+frostlib_hash + "\n")
 f.close
+
+us = raw_input("Revision ändern?")
+ja = ["ja", "yes", "1"]
+if us in ja:
+    f = open("frostlib/__init__.py", 'r')
+    data = f.readlines()
+    f.close()
+    f = open("frostlib/__init__.py", 'w')
+    for lines in data:
+        if not "__REVISION__" in lines:
+            f.write(lines)
+        else:
+            a = lines
+            a = a.split()
+            revision = a[2]
+            f.write("__REVISION__ = "+ str(int(revision) + 1) + "\n")
+    f.close
+    print "Revision set to " + str(int(revision) + 1)
