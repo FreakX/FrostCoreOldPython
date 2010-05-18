ca4261f7375e6ec9619c3ffa21103c250a88cb12
FreakX <dani11795@googlemail.com>
2010-05-18 15:34:56 +0200

    [11] Fix Hashchanger
    Signed-off-by: FreakX <dani11795@googlemail.com>

--- hashchanger.py
+++ hashchanger.py
@@ -1,27 +1,5 @@
 # -*- coding: cp1252 -*-
 import frostlib
-frostlib_hash = frostlib.hash.GetHashofDirs("frostlib", 0)
-f = open("logon.cfg", 'r')
-data = f.readlines()
-f.close()
-f = open("logon.cfg", 'w')
-for lines in data:
-    if not "frostlib_hash" in lines:
-        f.write(lines)
-    else:
-        f.write("frostlib_hash = "+frostlib_hash + "\n")
-f.close
-
-f = open("world.cfg", 'r')
-data = f.readlines()
-f.close()
-f = open("world.cfg", 'w')
-for lines in data:
-    if not "frostlib_hash" in lines:
-        f.write(lines)
-    else:
-        f.write("frostlib_hash = "+frostlib_hash + "\n")
-f.close
 
 us = raw_input("Revision ändern?")
 ja = ["ja", "yes", "1"]
@@ -40,4 +18,29 @@ if us in ja:
             f.write("__REVISION__ = "+ str(int(revision) + 1) + "\n")
     f.close
     print "Revision set to " + str(int(revision) + 1)
+
+frostlib_hash = frostlib.hash.GetHashofDirs("frostlib", 0)
+f = open("logon.cfg", 'r')
+data = f.readlines()
+f.close()
+f = open("logon.cfg", 'w')
+for lines in data:
+    if not "frostlib_hash" in lines:
+        f.write(lines)
+    else:
+        f.write("frostlib_hash = "+frostlib_hash + "\n")
+f.close
+print "Set Logon Hash to: " + str(frostlib_hash)
+f = open("world.cfg", 'r')
+data = f.readlines()
+f.close()
+f = open("world.cfg", 'w')
+for lines in data:
+    if not "frostlib_hash" in lines:
+        f.write(lines)
+    else:
+        f.write("frostlib_hash = "+frostlib_hash + "\n")
+f.close
+print "Set World Hash to: " + str(frostlib_hash)
+
 pause = raw_input("Taste drücken zum Beenden ...")
